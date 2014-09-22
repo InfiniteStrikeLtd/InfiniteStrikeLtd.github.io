df3a544139a8a7b4788f4f0b838fbacbd50a9d75
InfiniteStrike <dravenlewis.is@outlook.com>
2014-09-22 12:48:53 -0500

    added social links

---
+++ JavaScript/main.js
@@ -0,0 +1,10 @@
+
+// when the document (website) is ready run a unamed 
+$(document).ready(function(){
+    //when you click on IE_Close (The x) do the below
+   $('#IE_Close').click(function(){
+    //$('#IE_Close').remove();
+    // remove the element form the document
+    $('#IE_Waring').remove();   
+   })
+});
