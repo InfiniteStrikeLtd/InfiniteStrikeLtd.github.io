3ee4282db24cecb9d57ff4ce4359562c59c355a7
InfiniteStrike <dravenlewis.is@outlook.com>
2014-09-23 14:17:22 -0500

    update

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,8 +3,14 @@
 $(document).ready(function(){
     //when you click on IE_Close (The x) do the below
    $('#IE_Close').click(function(){
-    //$('#IE_Close').remove();
-    // remove the element form the document
-    $('#IE_Waring').remove();   
+    closePopup();  
    })
+   
+     
+       
+    
+    var closePopup = function(){
+        $('#IE_Waring').remove(); 
+   }
+   
 });
