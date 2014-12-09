8e35de2c4d6524ca6782be90e2a943b34787bda0
InfiniteStrike <dravenlewis.is@outlook.com>
2014-12-09 11:38:22 -0600

    Implemented Style Changiner

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -149,4 +149,21 @@ function m(e,t){return e+t&4294967295}var a="0123456789abcdef".split("");return 
  
  
  setNewIntro();
+    
+    
+$(window).bind("resize", resizeWindow);
+		function resizeWindow(e){
+			var newWindowWidth = $(window).width();
+            // If width width is below 600px, switch to the mobile stylesheet
+			 if(newWindowWidth < 600){ 				
+                $("link[rel=stylesheet]").attr({href : "http://infinitestrikeltd.github.io/mobile.css"});
+                 closePopup();
+                } 			
+                // Else if width is above 600px, switch to the large stylesheet 
+                else if(newWindowWidth > 600){
+				$("link[rel=stylesheet]").attr({href : "http://infinitestrikeltd.github.io/style.css"});
+			}
+		}    
+    
+    
 });
