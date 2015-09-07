dcc3aade250abc6fb674d8f6a3a722f5d6c40ff5
infinitestrike <dravenlewis.is@outlook.com>
2015-09-07 06:49:53 -0500

    Updated Coverpage and added icons for site use sent from Netbeans

--- JS/Article.js
+++ JS/Article.js
@@ -5,5 +5,5 @@
  date:     "Fri Jul 31 22:48:11 CDT 2015", 
  title:    "StrikeUI Update and Tonnes more!", 
  subtitle: "",
- content:  "After a long hiatis infinite strike is back and better than ever with the new StrikUI system inspired by bootstrap; but the cool thing is <strong>no bootstrap libraries were used.</strong> so that means that this is all our stuff and it looks SOO cool and we welcome you back to infinite strike and the new Strike UI" 
+ content:  "After a long time off, Infinite Strike is back and better than ever with the new StrikeUI system inspired by bootstrap; but the cool thing is <strong>no bootstrap libraries were used.</strong> so that means that this is all our stuff and it looks SOO cool. We welcome you back to infinite strike and the new Strike UI" 
  };
