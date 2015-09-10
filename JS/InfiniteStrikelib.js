954a92b2cd4c80f5b75648c96d69acd5317dded1
dravenlewis <>
2015-09-10 11:58:53 -0500

    added blocking for IE

--- JS/InfiniteStrikelib.js
+++ JS/InfiniteStrikelib.js
@@ -25,6 +25,33 @@ function main(){
     upDateGame();
     showNews();
      
+     
+    
+  function getInternetExplorerVersion(){
+  var rv = -1;
+  if (navigator.appName == 'Microsoft Internet Explorer')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  else if (navigator.appName == 'Netscape')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  return rv;
+}
+
+if(getInternetExplorerVersion() !== -1){
+   window.location = "http://infinitestrikeltd.github.io/Error.html?errType=IEError";
+   
+}
+ 
+     
     // Set up the footer
     $('.footer').append('<div class = "linkContainer"></div>');
     for(i = 0; i < footerList.length; i++){
