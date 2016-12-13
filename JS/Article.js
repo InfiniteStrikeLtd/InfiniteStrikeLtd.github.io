8e6b558525091f4405c0a0cc9b12c5f872e5efa2
Draven Lewis <dravenlewis.is@outlook.com>
2016-12-13 14:01:28 -0600

    Update Article.js

--- JS/Article.js
+++ JS/Article.js
@@ -2,14 +2,9 @@
   // Generated on Fri Jul 31 22:48:11 CDT 2015 
  var newsContext = {
  author:    "Draven Lewis @draven_lewis_is",
- date:     "Thur Apr 7 1:38:06 CDT 2016", 
- title:    "We're interested in what you think.", 
+ date:     "Tues Dec 13 1:38:06 CDT 2016", 
+ title:    "Busy... Hmmm", 
  subtitle: "",
- content:  "While looking at our Google Analytics, I've noticed that we are getting tons of new visitors;<br/>"+ 
-			"approximately 59.5% of all our visitors are new visitors. As well as 40.5% of visitors who are<br/>"+
-			"returning visitors. So I want to know what you guys think, why you're here, and why/how you use<br/>"+
-			"our site. I ask for a small moment of your time to take a survey for us to better tailor our site<br/>"+ 
-			"around your interests.<br/><br/>Results of the Survey will be compiled and evaluated on 4/30/16<br/><br/>"+
-			"<a href = \"https://www.surveymonkey.com/r/26BQFRT\" target = \"_blank\">Take the Survey</a><br/>"
+ content:  "Infinite strike is a little busy sorry for the wait..."
 
  };
